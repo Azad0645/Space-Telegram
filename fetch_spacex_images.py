d07a5fe64430f65ad799a5b9106be8c3e5e2a300
Azad0645 <fenix16450@gmail.com>
2025-02-11 00:04:01 +0300

    Set default "latest" for SpaceX launch ID

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -2,19 +2,15 @@ import argparse
 from utils import fetch_json, download_images
 
 
-def get_spacex_image_urls(launch_id=None):
-    url = "https://api.spacexdata.com/v5/launches"
-    if launch_id:
-        url = f"{url}/{launch_id}"
-    else:
-        url = f"{url}/latest"
+def get_spacex_image_urls(launch_id="latest"):
+    url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
     json_response = fetch_json(url)
     return json_response['links']['flickr']['original']
 
 
 def main():
     parser = argparse.ArgumentParser(description="Fetch SpaceX launch images.")
-    parser.add_argument("--launch_id", type=str, help="ID of the SpaceX launch")
+    parser.add_argument("--launch_id", type=str, default="latest", help="ID of the SpaceX launch")
     parser.add_argument("--folder", type=str, default="spacex_images", help="Folder to save SpaceX images")
     args = parser.parse_args()
 
